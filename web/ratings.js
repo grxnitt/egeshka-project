436aae57d037cd694b6988ed6772d64289ca109d
grxnitt <starod222s@mail.ru>
2026-09-20 17:14:50 +0300

    Align Insperia card and teacher copy

--- web/ratings.js
+++ web/ratings.js
@@ -1,4 +1,4 @@
-import { decisionFields, schoolContent, schoolFilters } from './school-content.js?v=26';
+import { decisionFields, schoolContent, schoolFilters } from './school-content.js?v=27';
 import { applyLiveRatings, ratingBreakdown, ratingMark, teacherRatingLabel } from './supabase-client.js?v=4';
 const $ = selector => document.querySelector(selector);
 const escape = value => String(value).replace(/[&<>"']/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
@@ -16,7 +16,7 @@ const criterionDetails = [
 ];
 const methodologyCriteria = () => criterionDetails.map(([weight, title, description]) => `<article class="weight-item"><b>${weight}</b><span>${title}</span><small>${description}</small></article>`).join('');
 import { labels, quizSubjectKeys, teacherSubjects } from './subjects.js?v=1';
-const schoolLogos = {'Умскул':'assets/schools/umskul.svg','100балльный репетитор':'assets/schools/100points.svg','Сотка':'assets/schools/sotka.webp','Фоксфорд':'assets/schools/foxford.ico','Вебиум':'assets/schools/webium.ico','99 Баллов':'assets/schools/99ballov.ico','Турбо ЕГЭ':'assets/schools/turbo.png','ЕГЭLand':'assets/schools/egeland.png','СМИТАП':'assets/schools/smitup.svg','PARTA':'assets/schools/parta.svg','Skysmart':'assets/schools/skysmart.svg','Школково':'assets/schools/shkolkovo.ico','ЕГЭ Налегке':'assets/schools/egenalegke.png','StudyCats':'assets/schools/studycats.png','NeoFamily':'assets/schools/neofamily.ico','Морозилка':'assets/schools/morozilka.png'};
+const schoolLogos = {'Умскул':'assets/schools/umskul.svg','100балльный репетитор':'assets/schools/100points.svg','Сотка':'assets/schools/sotka.webp','Фоксфорд':'assets/schools/foxford.ico','Вебиум':'assets/schools/webium.ico','99 Баллов':'assets/schools/99ballov.ico','Турбо ЕГЭ':'assets/schools/turbo.png','ЕГЭLand':'assets/schools/egeland.png','СМИТАП':'assets/schools/smitup.svg','PARTA':'assets/schools/parta.svg','Skysmart':'assets/schools/skysmart.svg','Школково':'assets/schools/shkolkovo.ico','ЕГЭ Налегке':'assets/schools/egenalegke.png','StudyCats':'assets/schools/studycats.png','NeoFamily':'assets/schools/neofamily.ico','Морозилка':'assets/schools/morozilka.png','Инсперия':'assets/schools/insperia.svg'};
 const dialog = $('#detail-dialog');
 const fromQuiz = new URLSearchParams(location.search).get('from') === 'quiz';
 const quizReturn = '<a class="quiz-return" href="index.html?resume=quiz">← Вернуться к подбору</a>';
